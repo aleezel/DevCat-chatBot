b137b0db22dcdcc62891cd140abeb03e29b7497e
Ale <alejandra.piedra02@gmail.com>
2024-01-11 23:19:50 -0600

    Ultima parte de la pasantía. Se agregan las imágenes que mandará el bot por mensaje

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -7,8 +7,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.util.Map;
 
-import static es.codegym.telegrambot.TelegramBotContent.STEP_1_TEXT;
-import static es.codegym.telegrambot.TelegramBotContent.STEP_2_TEXT;
+import static es.codegym.telegrambot.TelegramBotContent.*;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
@@ -24,6 +23,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         //En el caso de que el texto recibido de getMessageText() sea /start.
         if(getMessageText().equals("/start")){
             setUserGlory(0);
+            sendPhotoMessageAsync("step_1_pic");
             sendTextMessageAsync(STEP_1_TEXT,
                     //Tipo de objeto que guarda información Key Value
                     Map.of("Hackear la nevera", "step_1_btn"));
@@ -31,9 +31,67 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
         if (getCallbackQueryButtonKey().equals("step_1_btn")){
             setUserGlory(20);
+            sendPhotoMessageAsync("step_2_pic");
             sendTextMessageAsync(STEP_2_TEXT,
-                    Map.of());
+                    Map.of("¡Toda una salchica! +20 de fama", "step_2_btn",
+                            "¡Toma un pescado! +20 de fama", "step_2_btn",
+                            "¡Tirar una lata de pepinillos! +20 de fama", "step_2_btn"));
         }
+
+        if (getCallbackQueryButtonKey().equals("step_2_btn")){
+            setUserGlory(20);
+            sendPhotoMessageAsync("step_3_pic");
+            sendTextMessageAsync(STEP_3_TEXT,
+                    Map.of("Hackear al robot aspirador", "step_3_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_3_btn")){
+            setUserGlory(30);
+            sendPhotoMessageAsync("step_4_pic");
+            sendTextMessageAsync(STEP_4_TEXT,
+                    Map.of("¡Enviar al robot aspirador a por comida! +30 de fama", "step_4_btn",
+                            "¡Dar un paseo en el robot aspirador! +30 de fama", "step_4_btn",
+                            "¡Huir del robot aspirador! +30 de fama", "step_4_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_4_btn")){
+            setUserGlory(40);
+            sendPhotoMessageAsync("step_5_pic");
+            sendTextMessageAsync(STEP_5_TEXT,
+                    Map.of("Equiparse la GoPro abandonada +40 de fama", "step_5_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_5_btn")){
+            setUserGlory(30);
+            sendPhotoMessageAsync("step_6_pic");
+            sendTextMessageAsync(STEP_6_TEXT,
+                    Map.of("Atún con especias para la cena +30 de fama", "step_6_btn",
+                            "Leche calentita con cobijitas +30 de fama", "step_6_btn",
+                            "¡Un castillo gatuno! +30 de fama", "step_6_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_6_btn")){
+            setUserGlory(20);
+            sendPhotoMessageAsync("step_7_pic");
+            sendTextMessageAsync(STEP_7_TEXT,
+                    Map.of("¡A con las garras a hackear la computadora!", "step_7_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_7_btn")){
+            setUserGlory(50);
+            sendPhotoMessageAsync("step_8_pic");
+            sendTextMessageAsync(STEP_8_TEXT,
+                    Map.of("¿Cómo termina este ajetreado catuno día?", "step_8_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_8_btn")){
+            setUserGlory(50);
+            sendPhotoMessageAsync("final_pic");
+            sendTextMessageAsync(FINAL_TEXT,
+                    Map.of("¿Qué más aventuras deparan a este gatito?", "step_Finalme _btn"));
+        }
+
+
     }
 
     public static void main(String[] args) throws TelegramApiException {
